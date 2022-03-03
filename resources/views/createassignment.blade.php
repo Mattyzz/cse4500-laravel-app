ad8604405fa9638b7b00cc79fc83205dd8cbdffb
Matthew De LA Cruz <mdc0195@gmail.com>
2022-03-02 18:34:52 -0800

    DB Stuff added

---
+++ resources/views/createassignment.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Board')
+
+@section('content_header')
+    <h1>Create New Assignment</h1>
+@stop
+
+@section('content')
+    <form>
+        
+    </form>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
