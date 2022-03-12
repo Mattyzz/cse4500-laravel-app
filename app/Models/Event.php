b2d44732a15a536c5cd849e58244290331b1b55c
Matthew De La Cruz <mdc0195@gmail.com>
2022-03-11 20:46:19 -0800

    Borke

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    protected $fillable =['title', 'start_at', 'end_at'];
+    protected $fillable = ['title', 'start_at', 'end_at'];
     protected $table = 'events';
     use HasFactory;
 }
